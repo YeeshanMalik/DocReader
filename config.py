cd02ae67c44f176d021f42d531530765ba0ea01b
Yeeshan Malik <159174599+YeeshanMalik@users.noreply.github.com>
2025-08-15 03:19:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
 import google.generativeai as genai
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import os
+
 
 PERSIST_DIR = "./chroma_store"
 UPLOAD_DIR = "./uploads"
@@ -10,7 +13,10 @@ Path(UPLOAD_DIR).mkdir(parents=True, exist_ok=True)
 # Models
 EMBEDDINGS_MODEL = "text-embedding-004"
 LLM_MODEL = "gemini-1.5-pro"
-
+embeddings = GoogleGenerativeAIEmbeddings(
+    model=EMBEDDINGS_MODEL,
+    google_api_key=os.getenv("GEMINI_API_KEY")
+)
 # Configure Gemini
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
@@ -31,5 +37,6 @@ def call_gemini(prompt):
     model = genai.GenerativeModel(LLM_MODEL)
     response = model.generate_content(prompt)
     return response.text
+
 
 
